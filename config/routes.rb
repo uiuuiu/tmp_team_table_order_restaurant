50e4ea80ebe9c5e5c44e08439afb693535c00e51
at-dypham <dy.pham@asiantech.vn>
2018-04-21 17:16:33 +0700

    [Add] root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: 'home#index'
   resources :home
   resources :tables
   resources :menu_grid
